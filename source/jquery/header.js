9ed03ea9eb810ce455ce9659854bdf2ebe14feac
FuryVangel <edgecity23@gmail.com>
2021-02-20 16:59:27 +0300

    Remove

--- source/jquery/header.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-        $('.btn-trigger').click(function () {
-                $('.btn-trigger, .menu__primary, .menu__overlay').toggleClass('active');
-        });
-
-        $('.sub-menu').click(function() {
-                $(this).toggleClass('active');
-        });
-});
